737b116b2a0b55444be0bd670b84e5ca174d2a61
AdmaMotta <admafui@hotmail.com>
2024-09-16 20:49:16 -0300

    Initial commit

---
+++ src/main/java/dio/me/RestSpringWeb/RestSpringWebApplication.java
@@ -0,0 +1,13 @@
+package dio.me.RestSpringWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSpringWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSpringWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dio/me/RestSpringWeb/RestSpringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.me.RestSpringWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
